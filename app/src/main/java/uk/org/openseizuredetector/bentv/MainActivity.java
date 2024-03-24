acbad044d0c92e2dd6d8f16cfc9780417399048c
Graham Jones <grahamjones139@gmail.com>
2024-03-24 21:42:40 +0000

    Re-wrote from scratch to use vlc as media player

---
+++ app/src/main/java/uk/org/openseizuredetector/bentv/MainActivity.java
@@ -0,0 +1,75 @@
+package uk.org.openseizuredetector.bentv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.util.VLCVideoLayout;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private static final boolean USE_TEXTURE_VIEW = false;
+    private static final boolean ENABLE_SUBTITLES = true;
+    private static final String ASSET_FILENAME = "bbb.m4v";
+
+    private VLCVideoLayout mVideoLayout = null;
+
+    private LibVLC mLibVLC = null;
+    private MediaPlayer mMediaPlayer = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        final ArrayList<String> args = new ArrayList<>();
+        args.add("-vvv");
+        mLibVLC = new LibVLC(this, args);
+        mMediaPlayer = new MediaPlayer(mLibVLC);
+
+        mVideoLayout = findViewById(R.id.video_layout);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMediaPlayer.release();
+        mLibVLC.release();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mMediaPlayer.attachViews(mVideoLayout, null, ENABLE_SUBTITLES, USE_TEXTURE_VIEW);
+
+        try {
+            final Media media = new Media(mLibVLC, Uri.parse("rtsp://192.168.0.6/play2.sdp"));
+            mMediaPlayer.setMedia(media);
+            media.release();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception opening steam"+e.toString());
+        }
+        mMediaPlayer.play();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mMediaPlayer.stop();
+        mMediaPlayer.detachViews();
+    }
+}
